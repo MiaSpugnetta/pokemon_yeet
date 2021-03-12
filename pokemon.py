e7af7f390619e0d4a6be70b7f326aabc3fc37cc1
MiaSpugnetta <martina.figlia@gmail.com>
2021-03-12 18:12:54 +0100

    imporved readability pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -16,7 +16,7 @@ class Pokemon:
 
     # print hp
     def get_status(self):
-        print(f"{self.name}: HP: {self.hp} STATUS: {self.status} TYPE: {self.type}")
+        print(f"{self.name}: HP: {self.hp} TYPE: {self.type}")#STATUS: {self.status}")
 
     # print move list
     def get_attacks(self):
@@ -28,7 +28,6 @@ class Pokemon:
         damage = self.attacks[attack] * weaknesses[self.type][adversary.type]
         print(f"{self.name} attacked with {attack} and it did {damage} damage")
         #time.sleep(1)
-
         return damage
 
     def receive_attack(self, damage):
@@ -41,12 +40,6 @@ class Lineup:
     def __init__(self):
         self.members = []
 
-    def add_member(self, pokemon):
-        if len(self.members) < 3:
-            self.members.append(pokemon)
-        else:
-            print("Sorry Team Already Full")
-
     def show_lineup(self):
         for i in self.members:
             i.get_status()
@@ -57,8 +50,8 @@ class Lineup:
             rand_pokemon = random.randint(1, 151)  # select random pokemon
             poke_name, poke_type, poke_hp, moves = load_pokemon(rand_pokemon)  # fetch info from file
 
-            if poke_type == "fairy":  # change to stay true to I generation
-                poke_type = "normal"
+            if poke_type == "fairy":
+                poke_type = "normal"  # changed to stay true to I generation
 
             attacks = dict(zip(moves, damage))  # make a dict out of the list of moves from json and list of damage
             pokemon_to_add = Pokemon(poke_name, poke_type, attacks, poke_hp)#, "None" )
@@ -92,35 +85,29 @@ def game_logic():
     your_team.show_lineup()
     #time.sleep(2)
 
+    print("\n")
     print("PREPARE TO FIGHT!!!")
     print("\n")
     #time.sleep(3)
 
-    our_score = 0
+    our_score = 0  # ToDo: change score system to fight until all team pokemon have fainted
     enemy_score = 0
 
-
-# ToDo: figure out what happens when a pokemon that does 0 damage attacks first, have to manually interrupt
-# ToDo: change score system to fight until all team pokemon have fainted
     # There are as many matches as pokemon per team
     for i in range(len(your_team.members)):
         print(f"MATCH #{i+1}: {our_team.members[i].name} (HPs {our_team.members[i].hp}, {our_team.members[i].type}) vs {your_team.members[i].name} (HPs {your_team.members[i].hp}, {your_team.members[i].type})")
-        #print("\n")
         coin_flip = random.randrange(0, 2) # randomisation to see which pokemon attacks first
-        print(f"coin is {coin_flip}")
         if coin_flip == 0:
-            print("our pokemon starts")
+            print(f"coin is {coin_flip}, our pokemon starts")
             print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
         else:
-            print("enemy starts")
+            print(f"coin is {coin_flip}, enemy starts")
             print("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
         while your_team.members[i].hp > 0 and our_team.members[i].hp > 0:  # attack randomly until the pokemon is dead
             if coin_flip == 0:
-
                 damage = our_team.members[i].run_attack(your_team.members[i])
                 your_team.members[i].receive_attack(damage)
-                #time.sleep(1)
 
                 if your_team.members[i].hp <= 0:
                     print(f"{your_team.members[i].name} has FAINTED")
@@ -146,9 +133,8 @@ def game_logic():
                     continue
             else:
 
-                damage = your_team.members[i].run_attack(our_team.members[i]) 
+                damage = your_team.members[i].run_attack(our_team.members[i])
                 our_team.members[i].receive_attack(damage)
-                #time.sleep(1)
 
                 if our_team.members[i].hp <= 0:
                     print(f"{our_team.members[i].name} has FAINTED")
